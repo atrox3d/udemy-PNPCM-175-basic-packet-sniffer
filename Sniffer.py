e9f73eddbb9f48a18411e1ca658b02e985db46ca
atrox3d <atrox3d@gmail.com>
2021-06-23 12:30:52 +0200

    reformat code

--- Sniffer.py
+++ Sniffer.py
@@ -1,16 +1,17 @@
-#Please refer to the commented section below for a short Scapy recap!
+# Please refer to the commented section below for a short Scapy recap!
 
-#In Scapy, we will use the sniff() function to capture network packets.
-#To see a list of what functions Scapy has available, open Scapy and run the lsc() function.
-#Run the ls() function to see ALL the supported protocols.
-#Run the ls(protocol) function to see the fields and default values for any protocol. E.g. ls(BOOTP)
-#See packet layers and contents with the .show() method.
-#Dig into a specific packet layer using a list index: pkts[3][2].summary()
-#...the first index chooses the packet out of the pkts list, the second index chooses the layer for that specific packet.
-#Using the .command() method will return a string for the command necessary to recreate that sniffed packet.
+# In Scapy, we will use the sniff() function to capture network packets.
+# To see a list of what functions Scapy has available, open Scapy and run the lsc() function.
+# Run the ls() function to see ALL the supported protocols.
+# Run the ls(protocol) function to see the fields and default values for any protocol. E.g. ls(BOOTP)
+# See packet layers and contents with the .show() method.
+# Dig into a specific packet layer using a list index: pkts[3][2].summary()
+# ...the first index chooses the packet out of the pkts list,
+# the second index chooses the layer for that specific packet.
+# Using the .command() method will return a string for the command necessary to recreate that sniffed packet.
 
-#To see the list of optional arguments for the sniff() function:
-#print(sniff.__doc__)
+# To see the list of optional arguments for the sniff() function:
+# print(sniff.__doc__)
 '''
 Sniff packets and return a list of packets.
 
@@ -71,18 +72,16 @@ Examples:
   ...                                   pkt.summary()))
 '''
 
-
-#Importing the necessary modules
+# Importing the necessary modules
 import logging
 from datetime import datetime
 import subprocess
 import sys
 
-#This will suppress all messages that have a lower level of seriousness than error messages, while running or loading Scapy
+# This will suppress all messages that have a lower level of seriousness than error messages, while running or loading Scapy
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 logging.getLogger("scapy.interactive").setLevel(logging.ERROR)
 logging.getLogger("scapy.loading").setLevel(logging.ERROR)
-
 
 try:
     from scapy.all import *
@@ -90,18 +89,21 @@ try:
 except ImportError:
     print("Scapy package for Python is not installed on your system.")
     sys.exit()
-    
 
-#Printing a message to the user; always use "sudo scapy" in Linux!
+# Printing a message to the user; always use "sudo scapy" in Linux!
 print("\n! Make sure to run this program as ROOT !\n")
 
+# Asking the user for some parameters:
+# interface on which to sniff,
+# the number of packets to sniff,
+# the time interval to sniff, the protocol
 
-#Asking the user for some parameters: interface on which to sniff, the number of packets to sniff, the time interval to sniff, the protocol
 
-#Asking the user for input - the interface on which to run the sniffer
-net_iface = input("* Enter the interface on which to run the sniffer (e.g. 'enp0s8'): ")
+# Asking the user for input - the interface on which to run the sniffer
+iflist = ", ".join(get_if_list())
+net_iface = input(f"* Enter the interface on which to run the sniffer ({iflist}): ")
 
-#Setting network interface in promiscuous mode
+# Setting network interface in promiscuous mode
 '''
 Wikipedia: In computer networking, promiscuous mode or "promisc mode"[1] is a mode for a wired network interface 
 controller (NIC) or wireless network interface controller (WNIC) that causes the controller to pass all traffic 
@@ -110,94 +112,88 @@ intended to receive.
 This mode is normally used for packet sniffing that takes place on a router or on a computer connected to a hub.
 '''
 try:
-    subprocess.call(["ifconfig", net_iface, "promisc"], stdout = None, stderr = None, shell = False)
-
-except:
-    print("\nFailed to configure interface as promiscuous.\n")
-
+    subprocess.call(["ifconfig", net_iface, "promisc"], stdout=None, stderr=None, shell=False)
+except Exception as e:
+    print("\nFailed to configure interface as promiscuous.\n", repr(e))
+    exit()
 else:
-    #Executed if the try clause does not raise an exception
+    # Executed if the try clause does not raise an exception
     print("\nInterface %s was set to PROMISC mode.\n" % net_iface)
+exit()
 
-
-#Asking the user for the number of packets to sniff (the "count" parameter)
+# Asking the user for the number of packets to sniff (the "count" parameter)
 pkt_to_sniff = input("* Enter the number of packets to capture (0 is infinity): ")
 
-#Considering the case when the user enters 0 (infinity)
+# Considering the case when the user enters 0 (infinity)
 if int(pkt_to_sniff) != 0:
     print("\nThe program will capture %d packets.\n" % int(pkt_to_sniff))
-    
+
 elif int(pkt_to_sniff) == 0:
     print("\nThe program will capture packets until the timeout expires.\n")
 
-
-#Asking the user for the time interval to sniff (the "timeout" parameter)
+# Asking the user for the time interval to sniff (the "timeout" parameter)
 time_to_sniff = input("* Enter the number of seconds to run the capture: ")
 
-#Handling the value entered by the user
+# Handling the value entered by the user
 if int(time_to_sniff) != 0:
     print("\nThe program will capture packets for %d seconds.\n" % int(time_to_sniff))
-    
-    
-#Asking the user for any protocol filter he might want to apply to the sniffing process
-#For this example I chose three protocols: ARP, BOOTP, ICMP
-#You can customize this to add your own desired protocols
+
+# Asking the user for any protocol filter he might want to apply to the sniffing process
+# For this example I chose three protocols: ARP, BOOTP, ICMP
+# You can customize this to add your own desired protocols
 proto_sniff = input("* Enter the protocol to filter by (arp|bootp|icmp|0 is all): ")
 
-#Considering the case when the user enters 0 (meaning all protocols)
+# Considering the case when the user enters 0 (meaning all protocols)
 if (proto_sniff == "arp") or (proto_sniff == "bootp") or (proto_sniff == "icmp"):
     print("\nThe program will capture only %s packets.\n" % proto_sniff.upper())
-    
-elif (proto_sniff) == "0":
+
+elif proto_sniff == "0":
     print("\nThe program will capture all protocols.\n")
 
-
-#Asking the user to enter the name and path of the log file to be created
+# Asking the user to enter the name and path of the log file to be created
 file_name = input("* Please give a name to the log file: ")
 
-#Creating the text file (if it doesn't exist) for packet logging and/or opening it for appending
+# Creating the text file (if it doesn't exist) for packet logging and/or opening it for appending
 sniffer_log = open(file_name, "a")
 
 
-#This is the function that will be called for each captured packet
-#The function will extract parameters from the packet and then log each packet to the log file
+# This is the function that will be called for each captured packet
+# The function will extract parameters from the packet and then log each packet to the log file
 def packet_log(packet):
-    
-    #Getting the current timestamp
+    # Getting the current timestamp
     now = datetime.now()
-    
-    #Writing the packet information to the log file, also considering the protocol or 0 for all protocols
-    if proto_sniff == "0":
-        #Writing the data to the log file
-        print("Time: " + str(now) + " Protocol: ALL" + " SMAC: " + packet[0].src + " DMAC: " + packet[0].dst, file = sniffer_log)
-        
-    elif (proto_sniff == "arp") or (proto_sniff == "bootp") or (proto_sniff == "icmp"):
-        #Writing the data to the log file
-        print("Time: " + str(now) + " Protocol: " + proto_sniff.upper() + " SMAC: " + packet[0].src + " DMAC: " + packet[0].dst, file = sniffer_log)
 
-        
-#Printing an informational message to the screen
+    # Writing the packet information to the log file, also considering the protocol or 0 for all protocols
+    if proto_sniff == "0":
+        # Writing the data to the log file
+        print("Time: " + str(now) + " Protocol: ALL" + " SMAC: " + packet[0].src + " DMAC: " + packet[0].dst,
+              file=sniffer_log)
+    elif (proto_sniff == "arp") or (proto_sniff == "bootp") or (proto_sniff == "icmp"):
+        # Writing the data to the log file
+        print(
+            "Time: " + str(now) + " Protocol: " + proto_sniff.upper() + " SMAC: " + packet[0].src + " DMAC: " + packet[
+                0].dst, file=sniffer_log)
+
+
+# Printing an informational message to the screen
 print("\n* Starting the capture...")
 
-
-#Running the sniffing process (with or without a filter)
+# Running the sniffing process (with or without a filter)
 if proto_sniff == "0":
-    sniff(iface = net_iface, count = int(pkt_to_sniff), timeout = int(time_to_sniff), prn = packet_log)
+    sniff(iface=net_iface, count=int(pkt_to_sniff), timeout=int(time_to_sniff), prn=packet_log)
 
 elif (proto_sniff == "arp") or (proto_sniff == "bootp") or (proto_sniff == "icmp"):
-    sniff(iface = net_iface, filter = proto_sniff, count = int(pkt_to_sniff), timeout = int(time_to_sniff), prn = packet_log)
-    
+    sniff(iface=net_iface, filter=proto_sniff, count=int(pkt_to_sniff), timeout=int(time_to_sniff), prn=packet_log)
+
 else:
     print("\nCould not identify the protocol.\n")
     sys.exit()
 
-#Printing the closing message
+# Printing the closing message
 print("\n* Please check the %s file to see the captured packets.\n" % file_name)
 
-#Closing the log file
+# Closing the log file
 sniffer_log.close()
 
-
-
-#End of the program. 
-#Feel free to modify it, test it, add new protocols to sniff and improve de code whenever you feel the need to.
+# End of the program.
+# Feel free to modify it, test it, add new protocols to sniff and improve de code whenever you feel the need to.
